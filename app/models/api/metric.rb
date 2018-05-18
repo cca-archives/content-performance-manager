71530a7a38f6ba363e00bcc3938c5419351d9d23
Pablo Manrubia <pmanrubia@gmail.com>
2018-05-18 15:36:00 +0100

    Remove method `metric_names`
    We can now rely on accusers of the Metric class

--- app/models/api/metric.rb
+++ app/models/api/metric.rb
@@ -9,7 +9,7 @@ class Api::Metric
 
   attr_reader :metric, :from, :to, :content_id
 
-  validates :metric, presence: true, inclusion: { in: ::Metric.metric_names }
+  validates :metric, presence: true, inclusion: { in: ::Metric.find_all.map(&:name) }
   validates :from, presence: true, format: { with: DATE_REGEX, message: "Dates should use the format YYYY-MM-DD" }
   validates :to, presence: true, format: { with: DATE_REGEX, message: "Dates should use the format YYYY-MM-DD" }
   validates :content_id, presence: true, format: { with: CONTENT_ID_REGEX, message: "Content ID must be a UUID." }
